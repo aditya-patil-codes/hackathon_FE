f9d9f18b6d026c9b7e18270af3d49784c4a12d5e
Aditya Patil <aditya249645@gmail.com>
2024-11-13 01:14:22 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import ParticlesComponent from './components/particles';
+
+function App() {
+  return (
+    <div className="App">
+      <ParticlesComponent id="particles" />
+      <h1 className=' text-white text-center text-2xl'>Airavat Project :)</h1>
+    </div>
+  );
+}
+
+export default App;
